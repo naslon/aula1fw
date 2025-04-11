eb337346d3c0c2d9becfad04c9d1221bcb907fa2
Naslon <155392065+naslon@users.noreply.github.com>
2025-04-11 09:31:54 -0300

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Footer from "./componentes/footer";
+import Navbar from "./componentes/navbar";
+
+export default function home() {
+  return (
+    <div>
+      <Navbar />
+      <h1> Olá Mundo!</h1>
+      <Footer />
+    </div>
+  );
+}
